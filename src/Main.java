9afdd0dbd567b9a08a6fb6fc28caec184e02db8b
kukuh_wildan <kuwildansyah@gmail.com>
2023-03-13 12:01:53 +0700

    lala

--- src/Main.java
+++ src/Main.java
@@ -23,8 +23,8 @@ public class Main {
         Scanner input = new Scanner(System.in);
         boolean isBalik=true,ulang=true,isNumber = true;
         int pilihh;
-        double base;
-        double height;
+        double alas;
+        double tinggi;
         while(isBalik){
         System.out.println("=====LUAS SEGITIGA MASBRO====");
         System.out.println("1.Hasil Integer \n2.Hasil double\n3.Keluar");
@@ -33,18 +33,18 @@ public class Main {
             case 1:
                 while(ulang) {
                     System.out.print("Masukkan Alas: ");
-                    base = input.nextDouble();
-                    if (Double.isNaN(base)) {
+                    alas = input.nextDouble();
+                    if (Double.isNaN(alas)) {
                         System.out.println("inputanmu bukan angka");
                         ulang=true;
                     }
                     System.out.print("Masukkan Tinggi: ");
-                    height = input.nextDouble();
-                    if (Double.isNaN(height)) {
+                    tinggi = input.nextDouble();
+                    if (Double.isNaN(tinggi)) {
                         System.out.println("inputanmu bukan angka");
                         ulang=true;
                     }
-                    Segitiga<Double> segitigaint = new Segitiga<>(base, height);
+                    Segitiga<Double> segitigaint = new Segitiga<>(alas, tinggi);
                     System.out.println("Luas Segitiga as int: " + segitigaint.getResultAsInt());
                     System.out.println("\n Mau Hitung Lagi? 1. Ya \n 2. Gak dulu");
                     pilihh = input.nextInt();
@@ -61,19 +61,19 @@ public class Main {
             case 2:
                 while(ulang){
                 System.out.print("Masukkan Alas: ");
-                base = input.nextDouble();
-                    if (Double.isNaN(base)) {
+                alas = input.nextDouble();
+                    if (Double.isNaN(alas)) {
                         System.out.println("inputanmu bukan angka");
                         ulang=true;
                     }
                 System.out.print("Masukkan Tinggi: ");
-                height = input.nextDouble();
-                    if (Double.isNaN(height)) {
+                tinggi = input.nextDouble();
+                    if (Double.isNaN(tinggi)) {
                         System.out.println("inputanmu bukan angka");
                         ulang=true;
                     }
 
-                Segitiga<Double> segitigadouble = new Segitiga<>(base, height);
+                Segitiga<Double> segitigadouble = new Segitiga<>(alas, tinggi);
                 System.out.println("Luas Segita as double: " + segitigadouble.getResultAsDouble());
                 System.out.println("\n Mau Hitung Lagi? 1. Ya \n 2. Gak dulu");
                 pilihh= input.nextInt();
